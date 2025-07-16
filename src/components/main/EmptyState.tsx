69c3581ede1b011a4abe6eea7d728f99ad588785
rigelgfr <rigel.rahateen@student.president.ac.id>
2025-07-16 15:42:34 +0700

    style: bot icon animation

--- src/components/main/EmptyState.tsx
+++ src/components/main/EmptyState.tsx
@@ -7,7 +7,7 @@ export default function EmptyState() {
 
   return (
     <div className="flex flex-col justify-center items-center text-gray-400 mt-8">
-      <Bot className="w-16 h-16 sm:w-20 sm:h-20 mx-auto mb-4" />
+      <Bot className="w-16 h-16 sm:w-20 sm:h-20 mx-auto mb-2 text-aquamarine-50 dark:text-aquamarine animate-bounce" />
       <h1 className="text-lg sm:text-3xl font-semibold mb-1">
         Hello, <span className="text-foreground">{userName}!</span>
       </h1>
