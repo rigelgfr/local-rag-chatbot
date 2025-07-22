9a1b14a905d8831bc8ada793f8df88ef14cd186c
rigelgfr <rigel.rahateen@student.president.ac.id>
2025-07-22 16:54:02 +0700

    feat: logo as clickable link

--- src/components/custom-ui/Header.tsx
+++ src/components/custom-ui/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Bot } from "lucide-react";
 import { ThemeToggle } from "./ToggleUI";
 import ProfilePic from "./ProfilePic";
@@ -23,10 +24,10 @@ export default function Header() {
   return (
     <header className="bg-white dark:bg-background border-b border-gray-200 dark:border-aquamarine px-4 py-3">
       <div className="max-w-4xl mx-auto flex gap-2 items-center">
-        <div className="flex">
-          <Bot className="w-6 h-6 text-aquamarine-50 dark:text-aquamarine mr-2" />
+        <Link href="/" className="flex select-none">
+          <Bot className="w-6 h-6 text-aquamarine-50 dark:text-aquamarine mr-1" />
           <h1 className="text-xl font-semibold text-foreground">ALVA AI</h1>
-        </div>
+        </Link>
 
         <ThemeToggle className="ml-auto" />
         <Suspense fallback={<Skeleton className="h-8 w-8 rounded-full" />}>
